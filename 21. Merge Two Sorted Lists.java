62884ba804449ded1879481a4c3ddccf24cb8407
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2024-07-25 01:09:20 +0530

    Create 21. Merge Two Sorted Lists.java

---
+++ 21. Merge Two Sorted Lists.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
+        }
+
+        if (list1 != null) {
+            cur.next = list1;
+        } else {
+            cur.next = list2;
+        }
+
+        return dummy.next;        
+    }
+}
