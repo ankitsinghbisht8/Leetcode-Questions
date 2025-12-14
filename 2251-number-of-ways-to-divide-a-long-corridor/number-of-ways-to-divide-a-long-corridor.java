50bce3407ccc78a0f1953ac14b263f9b2d7c7996
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-14 23:13:14 +0530

    Time: 20 ms (92.80%) | Memory: 47.8 MB (53.60%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,25 +1,21 @@
 class Solution {
     public int numberOfWays(String corridor) {
-        final int MOD = 1_000_000_007;
+        final int MOD = 1000_000_007;
+        long result = 1;
+        int prevSeatIndex = 0;
+        int numSeats = 0;
 
-        int[][] count = new int[corridor.length() + 1][3];
-
-        count[corridor.length()][0] = 0;
-        count[corridor.length()][1] = 0;
-        count[corridor.length()][2] = 1;
-
-        for (int index = corridor.length() - 1; index >= 0; index--) {
-            if (corridor.charAt(index) == 'S') {
-                count[index][0] = count[index + 1][1];
-                count[index][1] = count[index + 1][2];
-                count[index][2] = count[index + 1][1];
-            } else {
-                count[index][0] = count[index + 1][0];
-                count[index][1] = count[index + 1][1];
-                count[index][2] = (count[index + 1][0] + count[index + 1][2]) % MOD;
+        for(int i = 0; i < corridor.length(); i++){
+            char c = corridor.charAt(i);
+            if(c == 'S'){
+                numSeats++;
+                if(numSeats > 2 && numSeats % 2 == 1){
+                    result = result * (i - prevSeatIndex)%MOD;
+                }
+                prevSeatIndex = i;
             }
         }
 
-        return count[0][0];
+        return numSeats > 1 && numSeats % 2 == 0 ? (int)result : 0;
     }
 }
