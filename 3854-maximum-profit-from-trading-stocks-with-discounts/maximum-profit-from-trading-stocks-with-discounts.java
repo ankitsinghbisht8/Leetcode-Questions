0464fc5210f943b6f4c29d97f33b69fce0055514
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-16 18:27:28 +0530

    Time: 347 ms (66.67%) | Memory: 47.2 MB (57.14%) - LeetSync

--- 3854-maximum-profit-from-trading-stocks-with-discounts/maximum-profit-from-trading-stocks-with-discounts.java
+++ 3854-maximum-profit-from-trading-stocks-with-discounts/maximum-profit-from-trading-stocks-with-discounts.java
@@ -1,88 +1,92 @@
-class solve {
-    
-    int[] dp0;
-    int[] dp1;
-    int size;
-
-    solve(int[] dp0, int[] dp1, int size) {
-        this.dp0 = dp0;
-        this.dp1 = dp1;
-        this.size = size;
-    }
-}
-
 class Solution {
-
-    public int maxProfit(
-        int n,
-        int[] present,
-        int[] future,
-        int[][] hierarchy,
-        int budget
-    ) {
-        List<Integer>[] g = new ArrayList[n];
+    private int[][][] dp;
+    private List<Integer>[] adList;
+    
+    public int maxProfit(int n, int[] present, int[] future, int[][] hierarchy, int budget) {
+        dp = new int[n][budget + 1][3];
+        adList = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            g[i] = new ArrayList<>();
-        }
-        for (int[] e : hierarchy) {
-            g[e[0] - 1].add(e[1] - 1);
+            adList[i] = new ArrayList<>();
         }
 
-        return dfs(0, present, future, g, budget).dp0[budget];
+        for (int[] h : hierarchy) {
+            adList[h[0] - 1].add(h[1] - 1);
+        }
+        
+        dfs(0, present, future, budget);
+        
+        int solution = 0;
+        for (int i = 0; i <= budget; i++) {
+            solution = Math.max(solution, Math.max(dp[0][i][0], dp[0][i][1]));
+        }
+        return solution;
     }
 
-    private solve dfs(
-        int u,
-        int[] present,
-        int[] future,
-        List<Integer>[] g,
-        int budget
-    ) {
-        int cost = present[u];
-        int dCost = present[u] / 2;
-        int[] dp0 = new int[budget + 1];
-        int[] dp1 = new int[budget + 1];
-        int[] subProfit0 = new int[budget + 1];
-        int[] subProfit1 = new int[budget + 1];
-        int uSize = cost;
+    private int getCost(int node, int curCost, int rep) {
+        if (rep == 0) {
+            return Math.max(dp[node][curCost][0], dp[node][curCost][1]);
+        } else if (rep == 1) {
+            return Math.max(dp[node][curCost][0], dp[node][curCost][2]);
+        } else {
+            return Math.max(dp[node][curCost][0], Math.max(dp[node][curCost][1], dp[node][curCost][2]));
+        }
+    }
 
-        for (int v : g[u]) {
-            solve childsolve = dfs(v, present, future, g, budget);
-            uSize += childsolve.size;
+    private void dfs(int v, int[] present, int[] future, int budget) {
+        if (budget <= 0) return;
 
-            for (int i = budget; i >= 0; i--) {
-                for (int sub = 0; sub <= Math.min(childsolve.size, i); sub++) {
-                    if (i - sub >= 0) {
-                        subProfit0[i] = Math.max(
-                            subProfit0[i],
-                            subProfit0[i - sub] + childsolve.dp0[sub]
-                        );
-                        subProfit1[i] = Math.max(
-                            subProfit1[i],
-                            subProfit1[i - sub] + childsolve.dp1[sub]
-                        );
+        int profit = future[v] - present[v];
+        int halfPresent = present[v] / 2;
+        int profitWithDiscount = future[v] - halfPresent;
+        
+        List<Integer> childNodes = adList[v];
+        int len = childNodes.size();
+        
+        for (int u : childNodes) {
+            dfs(u, present, future, budget);
+        }
+        
+        for (int rep = 0; rep < 3; rep++) {
+            int[][] childDp = new int[len + 1][budget + 1];
+            
+            for (int l = 0; l < len; l++) {
+                int node = childNodes.get(l);
+                int[] prevRow = childDp[l];
+                int[] currRow = childDp[l + 1];
+                
+                for (int b = 0; b <= budget; b++) {
+                    if (prevRow[b] == 0 && b > 0) continue;
+                    
+                    currRow[b] = Math.max(currRow[b], prevRow[b]);
+                    
+                    int maxChildBudget = budget - b;
+                    for (int cb = 0; cb <= maxChildBudget; cb++) {
+                        int childCost = getCost(node, cb, rep);
+                        if (childCost == 0 && cb > 0) continue;
+                        
+                        int newCost = prevRow[b] + childCost;
+                        if (newCost > currRow[b + cb]) {
+                            currRow[b + cb] = newCost;
+                        }
                     }
                 }
             }
-        }
-
-        for (int i = 0; i <= budget; i++) {
-            dp0[i] = subProfit0[i];
-            dp1[i] = subProfit0[i];
-            if (i >= dCost) {
-                dp1[i] = Math.max(
-                    subProfit0[i],
-                    subProfit1[i - dCost] + future[u] - dCost
-                );
+            
+            int[] lastRow = childDp[len];
+            if (rep == 0) {
+                for (int b = 0; b <= budget; b++) {
+                    dp[v][b][0] = lastRow[b];
+                }
+            } else if (rep == 1) {
+                int cost = present[v];
+                for (int b = cost; b <= budget; b++) {
+                    dp[v][b][1] = lastRow[b - cost] + profit;
+                }
+            } else {
+                for (int b = halfPresent; b <= budget; b++) {
+                    dp[v][b][2] = lastRow[b - halfPresent] + profitWithDiscount;
+                }
             }
-            if (i >= cost) {
-                dp0[i] = Math.max(
-                    subProfit0[i],
-                    subProfit1[i - cost] + future[u] - cost
-                );
-            }
         }
-
-        return new solve(dp0, dp1, uSize);
     }
 }
