29709c0d5ab8903190b0b52eb000c632fda4baee
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-18 23:36:31 +0530

    Time: 6 ms (50.00%) | Memory: 102.4 MB (35.53%) - LeetSync

---
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+        long[] profitSum = new long[n + 1];
+        long[] priceSum = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            profitSum[i + 1] = profitSum[i] + (long) prices[i] * strategy[i];
+            priceSum[i + 1] = priceSum[i] + prices[i];
+        }
+        long res = profitSum[n];
+        for (int i = k - 1; i < n; i++) {
+            long left = profitSum[i - k + 1];
+            long right = profitSum[n] - profitSum[i + 1];
+            long changeProfit = priceSum[i + 1] - priceSum[i - k / 2 + 1];
+            res = Math.max(res, left + changeProfit + right);
+        }
+        return res;
+    }
+}
