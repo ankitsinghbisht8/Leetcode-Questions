1e556601a10b48c6e31b0fe0a244e73a98f4409c
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-22 00:15:33 +0530

    Time: 1 ms (78.08%) | Memory: 43.6 MB (50.68%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -2,36 +2,31 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int n = strs.length;
         int m = strs[0].length();
-        char[][] a = new char[n][];
-        for (int i = 0; i < n; i++) {
-            a[i] = strs[i].toCharArray();
-        }
 
-        boolean[] resolved = new boolean[n - 1];
-
-        int unresolved = n - 1;
-
+        boolean[] sorted = new boolean[n - 1];
         int deletions = 0;
 
-        for (int col = 0; col < m && unresolved > 0; col++) {
+        for (int col = 0; col < m; col++) {
             boolean needDelete = false;
-
             for (int row = 0; row < n - 1; row++) {
-                if (!resolved[row] && a[row][col] > a[row + 1][col]) {
-                    needDelete = true;
-                    break;
+                if (!sorted[row]) {
+                    if (strs[row].charAt(col) > strs[row + 1].charAt(col)) {
+                        needDelete = true;
+                        break;
+                    }
                 }
             }
 
+            // If invalid, delete this column
             if (needDelete) {
                 deletions++;
                 continue;
             }
 
             for (int row = 0; row < n - 1; row++) {
-                if (!resolved[row] && a[row][col] < a[row + 1][col]) {
-                    resolved[row] = true;
-                    unresolved--;
+                if (!sorted[row] &&
+                    strs[row].charAt(col) < strs[row + 1].charAt(col)) {
+                    sorted[row] = true;
                 }
             }
         }
