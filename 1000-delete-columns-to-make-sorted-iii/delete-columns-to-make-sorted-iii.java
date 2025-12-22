943a30d730d2b028bd9c49f97c6fc652b691ed3b
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-22 21:58:52 +0530

    Time: 9 ms (20.51%) | Memory: 44.3 MB (47.44%) - LeetSync

--- 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int minDeletionSize(String[] s) {
-        int n = s.length;
-        int m = s[0].length();
-        int size = 0;
-        int[] dp = new int[m];
-
-        for (int i = 0; i < m; i++) {
-            dp[i] = 1;
-        }
-
-        int max_keep = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < i; j++) {
-                boolean valid = true;
-                for (int k = 0; k < n; k++) {
-                    if (s[k].charAt(j) > s[k].charAt(i)) {
-                        valid = false;
+    public int minDeletionSize(String[] strs) {
+        int m = strs.length;
+        int n = strs[0].length();
+        int[] dp = new int[n];
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            for(int j = 0 ; j < i; j++){
+                boolean isless = true;
+                for(int p = 0; p < m; p++){
+                    if(strs[p].charAt(i) < strs[p].charAt(j)){
+                        isless = false;
                         break;
                     }
                 }
-                if (valid) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if(isless){
+                    dp[i] = Math.max(dp[i], dp[j]);
                 }
             }
-            max_keep = Math.max(max_keep, dp[i]);
+            dp[i] ++;
+            min = Math.min(min, n - dp[i]);
         }
-
-        return m - max_keep;
+        return min;
+        
+        
     }
 }
