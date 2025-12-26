93568be4a2f18e5eb9e7f3fde1fcd8fca86e8232
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-27 00:38:00 +0530

    Time: 6 ms (99.83%) | Memory: 47 MB (48.79%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,28 +1,19 @@
 class Solution {
-
     public int bestClosingTime(String customers) {
-        int curPenalty = 0;
-        for (int i = 0; i < customers.length(); i++) {
-            if (customers.charAt(i) == 'Y') {
-                curPenalty++;
+        int n=customers.length();
+        int result=0;
+        int customerLeft=0;
+        for(int i=0;i<n;i++){
+            char ch=customers.charAt(i);
+            if(ch=='Y'){
+                customerLeft++;
+                if(customerLeft>0){
+                    result=i+1;
+                    customerLeft=0;
+                }
             }
+            else customerLeft--;
         }
-        int pen = curPenalty;
-        int earliest = 0;
-
-        for (int i = 0; i < customers.length(); i++) {
-            char ch = customers.charAt(i);
-            if (ch == 'Y') {
-                curPenalty--;
-            } else {
-                curPenalty++;
-            }
-            if (curPenalty < pen) {
-                earliest = i + 1;
-                pen = curPenalty;
-            }
-        }
-
-        return earliest;
+        return result;
     }
 }
