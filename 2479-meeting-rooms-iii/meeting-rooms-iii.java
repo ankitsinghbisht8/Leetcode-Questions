442a8a4247143f070f8f00ea856cccc5d644335d
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-28 01:38:45 +0530

    Time: 75 ms (41.11%) | Memory: 127.7 MB (75.47%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,43 +1,48 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        long[] roomavailable = new long[n];
-        int[] count = new int[n];
-        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
 
-        for (int[] meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
-            long minroomavailable = Long.MAX_VALUE;
-            int minAvailableTimeRoom = 0;
-            boolean foundUnusedRoom = false;
+        Arrays.sort(meetings,(a,b)->a[0]-b[0]);
+        
+        PriorityQueue<Integer> freeRooms = new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            freeRooms.add(i);
+        }
+        PriorityQueue<long[]> busyRooms=new PriorityQueue<>((a,b)->a[0]==b[0] ? (int)(a[1]-b[1]):(a[0]<b[0]? -1:1));
+        
+        int[] count=new int[n];
 
-            for (int i = 0; i < n; i++) {
-                if (roomavailable[i] <= start) {
-                    foundUnusedRoom = true;
-                    count[i]++;
-                    roomavailable[i] = end;
-                    break;
-                }
+        for(int[] meet:meetings){
+            long start=meet[0];
+            long end=meet[1];
+            long duration=end-start;
 
-                if (minroomavailable > roomavailable[i]) {
-                    minroomavailable = roomavailable[i];
-                    minAvailableTimeRoom = i;
-                }
+            while(!busyRooms.isEmpty() && busyRooms.peek()[0]<=start){
+                freeRooms.add((int) busyRooms.poll()[1]);
             }
 
-            if (!foundUnusedRoom) {
-                roomavailable[minAvailableTimeRoom] += end - start;
-                count[minAvailableTimeRoom]++;
+            if(!freeRooms.isEmpty()){
+                int room=freeRooms.poll();
+                count[room]++;
+                busyRooms.add(new long[]{end,room});
+            }
+            else{
+               long[] earliest=busyRooms.poll();
+               long newEnd=earliest[0] + duration;
+               int room=(int) earliest[1];
+
+               count[room]++;
+               busyRooms.add(new long[] {newEnd,room});
             }
         }
 
-        int maxcount = 0, maxcountRoom = 0;
-        for (int i = 0; i < n; i++) {
-            if (count[i] > maxcount) {
-                maxcount = count[i];
-                maxcountRoom = i;
+
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(count[i]>count[ans]){
+                ans=i;
             }
         }
-
-        return maxcountRoom;
+        
+        return ans;
     }
 }
