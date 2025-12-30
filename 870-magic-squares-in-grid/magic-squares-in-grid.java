2b7e204abd09bfe2c4d6a2331d1c9169c2d1c229
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-30 16:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (76.70%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i + 2 < rows; i++) {
+            for (int j = 0; j + 2 < cols; j++) {
+                if (magiccheck(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    boolean magiccheck(int[][] g, int r, int c) {
+        if (g[r + 1][c + 1] != 5) return false;
+
+        boolean[] seen = new boolean[10];
+
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int val = g[i][j];
+                if (val < 1 || val > 9 || seen[val]) return false;
+                seen[val] = true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (g[r + i][c] + g[r + i][c + 1] + g[r + i][c + 2] != 15)
+                return false;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (g[r][c + j] + g[r + 1][c + j] + g[r + 2][c + j] != 15)
+                return false;
+        }
+
+        if (g[r][c] + g[r + 1][c + 1] + g[r + 2][c + 2] != 15)
+            return false;
+
+        if (g[r][c + 2] + g[r + 1][c + 1] + g[r + 2][c] != 15)
+            return false;
+
+        return true;
+    }
+}
