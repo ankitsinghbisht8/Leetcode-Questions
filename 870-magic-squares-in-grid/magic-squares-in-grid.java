9db3ae5027cee8620d2414462186d1190afceeb0
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2025-12-30 16:33:34 +0530

    Time: 1 ms (44.89%) | Memory: 43.7 MB (14.20%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -1,48 +1,35 @@
 class Solution {
+    public boolean isMagic(int r, int c, int[][] grid){
+        int[] rowSum = {0,0,0};
+        int[] colSum = {0,0,0};
+        int diagSum = 0;
+        int antiDiagSum = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                int curr = grid[r+i][c+j];
+                if(curr < 1 || curr > 9 || set.contains(curr)) return false;
+                rowSum[i] += curr;
+                colSum[j] += curr;
+                if(i == j) diagSum += curr;
+                if(i + j == 2) antiDiagSum += curr;
+                set.add(curr);
+            }
+        }
+
+        return rowSum[0] == rowSum[1] && rowSum[1] == rowSum[2] && 
+        rowSum[0] == colSum[0] && colSum[0] == colSum[1] && colSum[1] == colSum[2]
+        && colSum[2] == diagSum && diagSum == antiDiagSum;
+    }
     public int numMagicSquaresInside(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int count = 0;
-
-        for (int i = 0; i + 2 < rows; i++) {
-            for (int j = 0; j + 2 < cols; j++) {
-                if (magiccheck(grid, i, j)) {
-                    count++;
-                }
+        int ans = 0;
+        for(int i = 0; i <= rows - 3; i++){
+            for(int j = 0; j <= cols - 3; j++){
+                if(isMagic(i,j,grid)) ans++;
             }
         }
-        return count;
-    }
-
-    boolean magiccheck(int[][] g, int r, int c) {
-        if (g[r + 1][c + 1] != 5) return false;
-
-        boolean[] seen = new boolean[10];
-
-        for (int i = r; i < r + 3; i++) {
-            for (int j = c; j < c + 3; j++) {
-                int val = g[i][j];
-                if (val < 1 || val > 9 || seen[val]) return false;
-                seen[val] = true;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (g[r + i][c] + g[r + i][c + 1] + g[r + i][c + 2] != 15)
-                return false;
-        }
-
-        for (int j = 0; j < 3; j++) {
-            if (g[r][c + j] + g[r + 1][c + j] + g[r + 2][c + j] != 15)
-                return false;
-        }
-
-        if (g[r][c] + g[r + 1][c + 1] + g[r + 2][c + 2] != 15)
-            return false;
-
-        if (g[r][c + 2] + g[r + 1][c + 1] + g[r + 2][c] != 15)
-            return false;
-
-        return true;
+        return ans;
     }
 }
