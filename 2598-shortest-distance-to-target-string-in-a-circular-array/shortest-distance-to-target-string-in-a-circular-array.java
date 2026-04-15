0121156d83a622c98c1abb93c2933f390740fce2
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2026-04-15 12:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (14.93%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        if (words[startIndex].equals(target)) {
-            return 0;
-        }
-        int moves = 0;
-        for (int i = 1; i < n/2+1; i++) {
-            moves++;
+        // if (words[startIndex].equals(target)) {
+        //     return 0;
+        // }
+        // int moves = 0;
+        for (int i = 0; i < n/2+1; i++) {
+            // moves++;
             if (words[(startIndex + i) % n].equals(target)) {
-                return moves;
+                return i;
             }
             if (words[(startIndex - i + n) % n].equals(target)) {
-                return moves;
+                return i;
             }
             // if (((startIndex + i) % n) == ((startIndex - i + n) % n) ||
             //         ((startIndex + i) % n) == ((startIndex - i + n) % n) + 1) {
