fccb73066f498b2afe8a93cb40d5c91ab2ef1187
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2026-06-02 23:18:50 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (77.78%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,43 +1,20 @@
 class Solution {
-
-    private int solve(
-        int[] start1,
-        int[] duration1,
-        int[] start2,
-        int[] duration2
-    ) {
-        int finish1 = Integer.MAX_VALUE;
-        for (int i = 0; i < start1.length; i++) {
-            finish1 = Math.min(finish1, start1[i] + duration1[i]);
-        }
-        int finish2 = Integer.MAX_VALUE;
-        for (int i = 0; i < start2.length; i++) {
-            finish2 = Math.min(
-                finish2,
-                Math.max(start2[i], finish1) + duration2[i]
-            );
-        }
-        return finish2;
+    public int earliestFinishTime(int[] ls, int[] ld, int[] ws, int[] wd) {
+        return Math.min(getans(ls, ld, ws, wd), getans(ws, wd, ls, ld));
     }
 
-    public int earliestFinishTime(
-        int[] landStartTime,
-        int[] landDuration,
-        int[] waterStartTime,
-        int[] waterDuration
-    ) {
-        int land_water = solve(
-            landStartTime,
-            landDuration,
-            waterStartTime,
-            waterDuration
-        );
-        int water_land = solve(
-            waterStartTime,
-            waterDuration,
-            landStartTime,
-            landDuration
-        );
-        return Math.min(land_water, water_land);
+    private int getans(int[] ls, int[] ld, int[] ws, int[] wd) {
+        int mini = Integer.MAX_VALUE;
+
+        for (int i = 0; i < ls.length; i++) {
+            mini = Math.min(mini, ls[i] + ld[i]);
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < ws.length; i++) {
+            ans = Math.min(
+                    ans,
+                    Math.max(mini, ws[i]) + wd[i]);
+        }
+        return ans;
     }
 }
