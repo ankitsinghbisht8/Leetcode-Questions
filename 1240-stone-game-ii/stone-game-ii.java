183a378c05e27857fe74471705ae84219222a731
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2026-08-09 22:50:54 +0530

    Time: 2 ms (100.00%) | Memory: 43.9 MB (97.33%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int stoneGameII(int[] piles) {
+        int[] suffixSum = Arrays.copyOf(piles, piles.length);
+
+        for (int i = suffixSum.length - 2; i >= 0; i--) {
+            suffixSum[i] += suffixSum[i + 1];
+        }
+        return maxStones(suffixSum, 1, 0, new int[piles.length][piles.length]);
+    }
+
+    private int maxStones(
+        int[] suffixSum,
+        int maxTillNow,
+        int currIndex,
+        int[][] memo
+    ) {
+        // If currIndex + 2*maxTillNow lies outside the array, pick all remaining stones.
+        if (currIndex + 2 * maxTillNow >= suffixSum.length) {
+            return suffixSum[currIndex];
+        }
+        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= 2 * maxTillNow; i++) {
+            res = Math.min(
+                res,
+                maxStones(
+                    suffixSum,
+                    Math.max(i, maxTillNow),
+                    currIndex + i,
+                    memo
+                )
+            );
+        }
+        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
+        return memo[currIndex][maxTillNow];
+    }
+}
