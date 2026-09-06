0311f907c8cda3e0a85266aca6b0e45916a0afb6
Ankit Singh Bisht <132978008+ankitsinghbisht8@users.noreply.github.com>
2026-09-06 23:15:37 +0530

    Time: 9 ms (6.23%) | Memory: 70 MB (91.86%) - LeetSync

---
+++ 4412-count-good-cyclic-rotations/count-good-cyclic-rotations.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int countGoodRotations(int[] nums) {
+        int n = nums.length;
+        int newarr[]= new int [n*2];
+        int ans=0;
+        for(int i=0;i<2*n;i++){
+            newarr[i] = nums[(i%n)];
+        }
+        long prefixarr[]=new long[2*n];
+        prefixarr[0]=nums[0];
+        for(int i=1;i<2*n;i++){
+            prefixarr[i]=prefixarr[i-1]+newarr[i];
+        }
+
+        for(int i=0;i<n;i++){
+            long lefthalf=0;
+            if(i-1>=0){
+                lefthalf = prefixarr[n/2+i-1] - prefixarr[i-1];
+            }
+            else{
+                lefthalf =  prefixarr[n/2+i-1];
+            }
+            long righthalf = prefixarr[i+n-1]-prefixarr[n/2+i-1];
+            // System.out.println("firsthalf "+lefthalf+"..lasthalf.."+righthalf);
+            if(lefthalf>righthalf){
+                ans++;
+            }
+        }
+        // for(int a:prefixarr){
+        //     System.out.print(a+"....");
+        // }
+        // for(int a:newarr){
+        //     System.out.print(a+"....");
+        // }
+        // for(int i=0;i<n;i++){
+        //     int firsthalf=0;
+        //     int lasthalf=0;
+        //     for(int j=i;j<i+half;j++){
+        //         firsthalf+=newarr[j];
+        //     }
+        //     for(int k=n-1+i;k>=i+half;k--){
+        //         lasthalf+=newarr[k];
+        //     }
+            // System.out.println("firsthalf "+firsthalf+"..lasthalf.."+lasthalf);
+            // if(firsthalf>lasthalf){
+            //     ans++;
+            // }
+        
+        return ans;
+    }
+}
